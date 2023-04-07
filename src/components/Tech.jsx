d498ae1f3dfeb61e26b48af71375530f542f8a51
ibra-kdbra <sonarfarouq@gmail.com>
2023-04-07 18:43:29 +0300

    styling and added the features for the ball animation

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,8 +6,16 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap
+    justify-center gap-10">
+      {technologies.map((technology)=>(
+        <div className="w-25 h-28 "
+        key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech,"")
