f4c00cec1bab68d1494dad3328a91f88f88eefc4
ibra-kdbra <git-repo-develop@outlook.com>
2025-08-06 23:55:27 +0300

    fixed spaghetti code with ts

---
+++ components/PlayerContainer.tsx
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion";
+import { slideIn } from "@/utils/motion";
+import { PlayerCanvas } from "./canvas";
+
+interface PlayerContainerProps {
+  isMobile: boolean;
+}
+
+function PlayerContainer({ isMobile }: PlayerContainerProps) {
+  return (
+    <motion.div
+      variants={slideIn("right", "tween", 0.2, 1)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true }}
+      className="md:w-1/3 w-full md:h-auto h-[440px] cursor-pointer"
+    >
+      <PlayerCanvas isMobile={isMobile} />
+    </motion.div>
+  );
+}
+
+export default PlayerContainer;
